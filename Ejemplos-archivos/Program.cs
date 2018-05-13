3e7022f15c5e8624349858f965c5205f5d9de9a2
Armando Rodríguez Vera <jarodriguezvera@gmail.com>
2018-05-12 21:34:53 -0500

    Ejemplo de la lectura de un archivo de forma aleatoria

---
+++ Ejemplos-archivos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace Ejemplos_archivos
+{
+    class Program
+    {
+        static void Main()
+        {
+            long offset;
+            int nextbyte;
+            // Archivo abcdh  :  "abcdefghijklmnopqrstuvwxyz"
+            FileStream fs = new FileStream(@"C:\Users\Armando\Dropbox\TP\abcdh.txt", FileMode.Open, FileAccess.Read);
+
+            for(offset = 1; offset <= fs.Length; offset++)
+            {
+                fs.Seek(-offset, SeekOrigin.End);
+                Console.Write(Convert.ToChar(fs.ReadByte()));
+            }
+
+            Console.WriteLine();
+
+            fs.Seek(20, SeekOrigin.Begin);
+
+            while((nextbyte=fs.ReadByte()) >0)
+            {
+                Console.Write(Convert.ToChar(nextbyte));
+            }
+            Console.WriteLine();
+
+
+
+
+        }
+    }
+}
